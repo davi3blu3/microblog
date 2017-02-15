999e06be1f81db8a287229213320e8ba3466c654
Dave Harned <dharned@gmail.com>
2017-02-14 21:11:57 -0600

    insert mongo data and test data retrieval

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ MongoClient.connect('mongodb://localhost:27017/microblog', function(err, db) {
     assert.equal(err, null);
     console.log('Connected to Mongo Database on 27017');
 
+    var coll = 'posts';
+
+    db.collection(coll).find().toArray(function(err, docs) {
+        assert.equal(err, null);
+        assert.notEqual(docs.length, 0);
+
+        docs.forEach(function(doc) {
+            console.log(doc.author.userName + " posted on " + doc.date);
+        })
+    })
+
 })
